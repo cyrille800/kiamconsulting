7950e9ec2f43f3088e7a826903ba39008b2e7227
romualdjunior <romualdmotcheho@yahoo.com>
2019-07-21 16:01:25 +0100

    romuald a fait un commit

---
+++ Frontend/js/index.js
@@ -0,0 +1,16 @@
+
+//  $(function(){
+//    $(".nav-link.dropdown-toggle").hover(function(){
+//     $(this).siblings(".dropdown-menu").css("display","block");
+//   },
+//   function(){
+//     $(this).siblings(".dropdown-menu").css("display","none");
+//   }
+// )
+
+
+        
+   
+       
+ 
+})
