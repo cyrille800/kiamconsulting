38b7e639b75d932975459a2f4382dcbece321207
armand cyrille <armandcyrilletakougangnzeukou@gmail.com>
2019-07-21 15:07:34 +0100

    first commit

---
+++ views/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+</head>
+<body style="background-color:1acf0;">
+	<div style="margin:auto;padding:auto;">
+		<h1>je veux tester</h1>
+	</div>
+</body>
+</html>
