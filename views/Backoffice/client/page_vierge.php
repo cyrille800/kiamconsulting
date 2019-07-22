c66a25c95d5d798af2ae14ae0778ed19dd621dc5
armand cyrille <armandcyrilletakougangnzeukou@gmail.com>
2019-07-22 21:06:59 +0100

    nouvelle modification

---
+++ views/Backoffice/client/page_vierge.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta name="description" content="Latest updates and statistic charts">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!--begin::Fonts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js">
+        </script>
+        <script>
+        WebFont.load({
+        google: {"families":["Poppins:300,400,500,600,700"]},
+        active: function() {
+        sessionStorage.fonts = true;
+        }
+        });
+        </script>
+
+        <link href="../../assets/Backoffice/vendors/global/vendors.bundle.css" rel="stylesheet" type="text/css" />
+        <link href="../../assets/Backoffice/css/demo4/style.bundle.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body style="padding:0px;margin:0px;">
+                    
+                            
+                                
+                                <!-- begin:: Subheader -->
+                                <div class="kt-subheader   kt-grid__item" id="kt_subheader" style="background-color:white;padding-left:50px;">
+                                    <div class="kt-subheader__main">
+                                        <h3 class="kt-subheader__title">
+                                        Bienvenu</h3>
+                                        <span class="kt-subheader__separator kt-hidden">
+                                        </span>
+                                        
+                                    </div>
+                                    <div class="kt-subheader__toolbar">
+                                        <div class="kt-subheader__wrapper">
+                                            <a href="#" class="btn btn-icon btn btn-label btn-label-brand btn-bold" data-toggle="kt-tooltip" title="Calendar" data-placement="top">
+                                                <i class="flaticon2-hourglass-1">
+                                                </i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- end:: Subheader -->
+                                
+                                <!-- begin:: Content -->
+                                <div class="kt-content kt-grid__item kt-grid__item--fluid" style="background-color:white;">
+                                    <!--begin::Dashboard 5-->
+                                    
+
+                    <center>
+                        page vierge
+                    </center>
+                                    
+                                    <!--end::Dashboard 5-->
+                                </div>
+                                <!-- end:: Content -->
+
+        <script src="../../assets/Backoffice/vendors/global/vendors.bundle.js" type="text/javascript">
+        </script>
+        <script src="../../assets/Backoffice/js/demo4/scripts.bundle.js" type="text/javascript">
+        </script>
+</body>
+</html>
